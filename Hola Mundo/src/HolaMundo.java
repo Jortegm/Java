9731adf18cbc64869394f9d7c0ff63a2f2ec6774
Jortegm <juanortegamarin@gmail.com>
2020-09-18 16:54:19 +0200

    UPDATE A JAVA

---
+++ Hola Mundo/src/HolaMundo.java
@@ -0,0 +1,13 @@
+
+public class HolaMundo {
+
+	/**
+	 * 
+	 * @author jortega
+	 */
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("Hola Mundo Con Java");
+	}
+
+}
